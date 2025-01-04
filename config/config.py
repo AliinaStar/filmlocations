0a24537f546ade44928619de17eb76388ec06925
AliinaStar <128737579+AliinaStar@users.noreply.github.com>
2025-01-04 04:12:21 +0200

    add .github/main_FilmSpot.yml

--- config/config.py
+++ config/config.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 import logging
 from urllib.parse import quote_plus
+from azure.identity import DefaultAzureCredential
+from azure.core.exceptions import ClientAuthenticationError
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,32 +14,34 @@ load_dotenv()
 
 Base = declarative_base()
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 
-required_env_vars = ['DB_USER', 'DB_PASS', 'DB_HOST', 'DB_NAME']
+required_env_vars = ['DB_HOST', 'DB_NAME', 'AZURE_CLIENT_ID', 'AZURE_CLIENT_SECRET', 'AZURE_TENANT_ID']
 missing_vars = [var for var in required_env_vars if not os.getenv(var)]
 if missing_vars:
     raise ValueError(f"Missing required environment variables: {', '.join(missing_vars)}")
 
-#connection_string = "mssql+pymssql://{DB_USER}:{DB_PASS}@{DB_HOST}:1433/{DB_NAME}"
-connection_string = "mssql+pyodbc://username:password@server.database.windows.net:1433/dbname?driver=ODBC+Driver+17+for+SQL+Server"
-'''connection_string = (
+# Get the AAD token
+try:
+    credential = DefaultAzureCredential()
+    token = credential.get_token("https://database.windows.net/.default")
+except ClientAuthenticationError as e:
+    logger.error(f"Authentication error: {e}")
+    raise
+
+# Ensure the driver name matches the installed ODBC driver
+connection_string = (
     f"DRIVER={{ODBC Driver 17 for SQL Server}};"
     f"SERVER={DB_HOST};"
     f"DATABASE={DB_NAME};"
-    f"UID={DB_USER};"
-    f"PWD={DB_PASS};"
-)'''
+    f"Authentication=ActiveDirectoryServicePrincipal;"
+    f"AccessToken={token.token}"
+)
 
 def get_engine():
     try:
-        encoded_connection_string = quote_plus(connection_string)
-        engine = create_engine(f"mssql+pyodbc:///?odbc_connect={encoded_connection_string}")
-        with engine.connect() as connection:
-            logger.info("Database connection successful!")
+        engine = create_engine(f"mssql+pyodbc:///?odbc_connect={quote_plus(connection_string)}")
         return engine
     except Exception as e:
         logger.error(f"Database connection error: {e}")
